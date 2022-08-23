06654320c74c74a231512d9200ed24be40759bc9
Brandon Berhent <bberhent@ascend-innovations.com>
2022-08-23 13:25:14 -0400

    New server

--- libs/utils/net/headers.go
+++ libs/utils/net/headers.go
@@ -6,7 +6,10 @@ import (
 
 // Get a clients real user IP Address
 func GetIPAddress(r *http.Request) string {
-	IPAddress := r.Header.Get("X-Real-Ip")
+	IPAddress := r.Header.Get("CF-Connecting-IP")
+	if IPAddress == "" {
+		IPAddress = r.Header.Get("X-Real-Ip")
+	}
 	if IPAddress == "" {
 		IPAddress = r.Header.Get("X-Forwarded-For")
 	}
--- libs/utils/net/headers_test.go
+++ libs/utils/net/headers_test.go
@@ -11,9 +11,15 @@ import (
 func TestGetIPAddressFromHeader(t *testing.T) {
 	ip := "123.45.67.89"
 
-	// 3 methods of getting IP Address, X-Real-Ip preferred, then X-Forwarded-For, then RemoteAddr
+	// 4 methods of getting IP Address, CF-Connecting-IP preferred, X-Real-Ip, then X-Forwarded-For, then RemoteAddr
 
 	request, _ := http.NewRequest(http.MethodPost, "appditto.com", bytes.NewReader([]byte("")))
+	request.Header.Set("CF-Connecting-IP", ip)
+	request.Header.Set("X-Real-Ip", "not-the-ip")
+	request.Header.Set("X-Forwarded-For", "not-the-ip")
+	utils.AssertEqual(t, ip, GetIPAddress(request))
+
+	request, _ = http.NewRequest(http.MethodPost, "appditto.com", bytes.NewReader([]byte("")))
 	request.Header.Set("X-Real-Ip", ip)
 	request.Header.Set("X-Forwarded-For", "not-the-ip")
 
