6f62e92eaf90473d2c90392d7b9c7bef85e040e2
Brandon Berhent <bbedward@gmail.com>
2022-11-04 18:44:07 -0400

    Normal logging

--- apps/server/src/database/postgres.go
+++ apps/server/src/database/postgres.go
@@ -6,7 +6,6 @@ import (
 	"github.com/bananocoin/boompow/apps/server/src/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 type Config struct {
@@ -23,9 +22,7 @@ func NewConnection(config *Config) (*gorm.DB, error) {
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		config.Host, config.Port, config.User, config.Password, config.DBName, config.SSLMode,
 	)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return db, err
 	}
